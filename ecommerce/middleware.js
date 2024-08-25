0de9dbdd4c1e453c010fe329b7f121f16f49b98e
ashukla333 <amanshukla3747@gmail.com>
2024-08-25 17:27:38 +0530

    order pages created

--- ecommerce/middleware.js
+++ ecommerce/middleware.js
@@ -12,5 +12,5 @@ export function middleware(request) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/cart","/wishlist","/checkout","/profile","product"],
+  matcher: ["/cart","/wishlist","/checkout","/profile","/product","/category"],
 };
